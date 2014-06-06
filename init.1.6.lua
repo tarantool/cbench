fa6dfffba8e54a167893bc65115357e4dead86ad
Alexandr <a.lyapunov@corp.mail.ru>
2014-06-06 14:53:31 +0400

    lua code compaction

--- init.1.6.lua
+++ init.1.6.lua
@@ -13,16 +13,4 @@ box.cfg {
 
 dofile('example.lua')
 
-json = require("json")
-
--- Encode the result and save to a file
-json_result = json.encode(result)
-filename = string.format('bench-result-%s-%s.json',
-    box.info.version, box.info.build.target);
-file = io.open(filename, 'w')
-file:write(json_result)
-file:flush()
-file:close()
-print('Benchmark result saved to ', filename)
-
 box.raise(0, "finish")
