b7eafde35f33baba38a49df140566f8785967171
Arseniy Antonov <arseny.antonov@gmail.com>
2018-03-19 11:07:00 +0300

    Removed microb integration from lua runner

--- cbench_runner.lua
+++ cbench_runner.lua
@@ -1,25 +1,18 @@
 -- init tarantool db wor tests
-box.cfg{
+box.cfg {
     slab_alloc_arena = 1,
     pid_file = 'tarantool.pid',
     wal_mode = 'none',
     snap_dir = '.',
-    work_dir = '.',    
+    work_dir = '.',
 }
 
 local bench = require('cbench')
-local http = require('http.client')
+
 
 -- settings
-local iters = 2
+local iterations = 2
 local count = 1000000
-local api_uri= 'http://sh5.tarantool.org/push'
-local token = arg[1]
-
-if token == nil then
-    print('Usage: ./cbench_runner.lua <api token>')
-    os.exit()
-end
 
 local version = box.info.version
 
@@ -27,7 +20,7 @@ function split(str, delim)
     if string.find(str, delim) == nil then
         return { str }
     end
-    local result,pat,lastpos = {},"(.-)" .. delim .. "()",nil
+    local result, pat, lastpos = {}, "(.-)" .. delim .. "()", nil
     for part, pos in string.gfind(str, pat) do
         table.insert(result, part)
         lastpos = pos
@@ -42,46 +35,40 @@ function urlencode(t)
         if result ~= '?' then
             result = result .. '&'
         end
-        result = result .. key..'='..val
+        result = result .. key .. '=' .. val
     end
     return result
 end
 
 -- Workloads
-local tests = {'replaces', 'selects', 'selrepl', 'updates', 'deletes'}
+local tests = { 'replaces', 'selects', 'selrepl', 'updates', 'deletes' }
 local workloads = {
-    {tests = tests, type = 'hash', parts = { 'num'}},
-    {tests = tests, type = 'hash', parts = { 'str' }},
-    {tests = tests, type = 'hash', parts = { 'num', 'num' }},
-    {tests = tests, type = 'hash', parts = { 'num', 'str'}},
-    {tests = tests, type = 'hash', parts = { 'str', 'num' }},
-    {tests = tests, type = 'hash', parts = { 'str', 'str' }},
-    {tests = tests, type = 'tree', parts = { 'num' }},
-    {tests = tests, type = 'tree', parts = { 'str' }},
-    {tests = tests, type = 'tree', parts = { 'num', 'num' }},
-    {tests = tests, type = 'tree', parts = { 'num', 'str' }},
-    {tests = tests, type = 'tree', parts = { 'str', 'num' }},
-    {tests = tests, type = 'tree', parts = { 'str', 'str' }}
+    { tests = tests, type = 'hash', parts = { 'num' } },
+    { tests = tests, type = 'hash', parts = { 'str' } },
+    { tests = tests, type = 'hash', parts = { 'num', 'num' } },
+    { tests = tests, type = 'hash', parts = { 'num', 'str' } },
+    { tests = tests, type = 'hash', parts = { 'str', 'num' } },
+    { tests = tests, type = 'hash', parts = { 'str', 'str' } },
+    { tests = tests, type = 'tree', parts = { 'num' } },
+    { tests = tests, type = 'tree', parts = { 'str' } },
+    { tests = tests, type = 'tree', parts = { 'num', 'num' } },
+    { tests = tests, type = 'tree', parts = { 'num', 'str' } },
+    { tests = tests, type = 'tree', parts = { 'str', 'num' } },
+    { tests = tests, type = 'tree', parts = { 'str', 'str' } }
 }
 
 function export(name, bench_key, value)
-    local parts = split(name:lower(), ' ')
     local chart_name = name:gsub(' ', '_'):gsub('+_', ''):lower()
     local result = {
-        key = token, name = 'cb.'..bench_key..'.'..chart_name,
-        param=tostring(math.floor(value)), unit='rps',
-        tab='cbench.' .. chart_name, v=version
+        key = token, name = 'cb.' .. bench_key .. '.' .. chart_name,
+        param = tostring(math.floor(value)), unit = 'rps',
+        tab = 'cbench.' .. chart_name, v = version
     }
-    local action_uri = api_uri .. urlencode(result)
-    local r = http.request('GET', action_uri)
-    if r.status ~= 200 then
-        print('Microb service error')
-        print('Status code =', r.status)
-    end
+    print(urlencode(result))
 end
 
 function run()
-    local benches = bench.run(workloads, count, iters)
+    local benches = bench.run(workloads, count, iterations)
     for _, data in pairs(benches) do
         local name = data[1]
         local series = data[2]
